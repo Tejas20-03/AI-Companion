1462de54aea936aedd75ae00a3c358795ae65ffc
Tejas20-03 <tejaschhabra.official@gmail.com>
2023-10-26 10:18:13 +0530

    Added Categories Filter

---
+++ scripts/seed.ts
@@ -0,0 +1,26 @@
+const {PrismaClient} = require("@prisma/client");
+
+const db = new PrismaClient();
+
+async function main() {
+    try {
+        await db.category.createMany({
+            data:[
+                {name:"Famous People"},
+                {name:"Movies and TV"},
+                {name:"Musicians"},
+                {name:"Games"},
+                {name:"Animals"},
+                {name:"Philosphy"},
+                {name:"Scientists"},
+                {name:"Batchmates"},
+            ]
+        })
+    } catch (error) {
+        console.error("Error seeding default categories",error);
+    }finally{
+        await db.$disconnect();
+    }
+};
+
+main();
